8b6e798aa916b7a04151c4914600a259eb70ce80
Giovanni Maria Zarrelli <148645923+starjunkk@users.noreply.github.com>
2023-12-13 17:22:54 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form</title>
+	<meta name="description" content="blabla">
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style_responsive.css">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="form-container">
+        <form action="#" method="post">
+            <div class="form-group row">
+                <label for="Date" class="col-10 col-form-label">Titolo*</label>
+                <div class="col-10">
+                    <input type="text" name="Title" id="Title" class="form-control" placeholder="Title" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="Cognome" class="col-10 col-form-label">Date*</label>
+                <div class="col-10">
+                    <input type="text" name="Date" id="Date" class="form-control" placeholder="Date" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="Content" class="col-10 col-form-label">Content</label>
+                <div class="col-10">
+                    <input type="text" name="Content" id="Content" class="form-control" placeholder="Content" required>
+                </div>
+            </div>
+        </form>
+</body>
+</html>
